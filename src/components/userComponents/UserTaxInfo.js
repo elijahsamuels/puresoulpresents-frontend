f94db3a8d4077173a984c6d15ea5de50ef9c9340
elijahsamuels <elijahsamuels@gmail.com>
2021-10-25 22:23:04 -0700

    built out values for UserTaxInfo

--- src/components/userComponents/UserTaxInfo.js
+++ src/components/userComponents/UserTaxInfo.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchUserData, editUser } from "../../actions/userActions";
+import { Controller, useFormContext } from "react-hook-form";
+import TextField from '@mui/material/TextField';
 
-function UserTaxInfo() {
+function UserTaxInfo(props) {
+
+  const { control, formState: { errors }} = useFormContext({
+    defaultValues: props.users.user
+  });
+  // console.log("props.users.user", props.users.user)
+
   return (
-    <div className="userDetails">
+    <div className="userTaxDetails">
       <h3>Tax Information</h3>
       <div width="100">
         <h6>
@@ -11,50 +21,136 @@ function UserTaxInfo() {
           receive a 1099 form via email.
         </h6>
       </div>
-      <p>
-        <label>
-          First Name
-          <input name="first_name" placeholder="First Name" />
-        </label>
 
-        <label>
-          Last Name
-          <input name="last_name" placeholder="Last Name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Address (line 1)
-          <input name="user_tax_address1" placeholder="Address (line 1)" />
-        </label>
+      <Controller name="tax_first_or_business_name" control={control} render={({ field }) => (
+          <TextField 
+            {...field} 
+            label="1. Name (as shown on your income tax form). Name is required. Do not leave blank."
+            variant="outlined"
+            placeholder="Name (as shown on your income tax form)"
+            size="small"
+            margin="dense"
+            error={!!errors.tax_first_or_business_name}
+            helperText={errors.tax_first_or_business_name ? errors.tax_first_or_business_name.message : ""}
+            />
+        )}/>
+        
+      <Controller name="tax_last_name" control={control} render={({ field }) => (
+          <TextField 
+            {...field} 
+            label="2. Legal Last Name"
+            variant="outlined"
+            placeholder="Legal last name"
+            size="small"
+            margin="dense"
+            error={!!errors.tax_last_name}
+            helperText={errors.tax_last_name ? errors.tax_last_name.message : ""}
+            />
+        )}/>
 
-        <label>
-          Address (line 2)
-          <input name="user_tax_address2" placeholder="Address (line 2)" />
-        </label>
-      </p>
-      <p>
-        <label>
-          City
-          <input name="user_tax_city" placeholder="City" />
-        </label>
-        <label>
-          State
-          <input name="user_tax_state" placeholder="State" />
-        </label>
-        <label>
-          Zip
-          <input name="user_tax_zip" placeholder="Zip" />
-        </label>
-      </p>
-      <p>
-        <label>
-          SSN/EIN
-          <input name="user_taxID" placeholder="123-45-6789" type="password" />
-        </label>
-      </p>
+      <Controller name="tax_address1" control={control} render={({ field }) => (
+          <TextField 
+            {...field} 
+            label="5. Address line 1"
+            variant="outlined"
+            placeholder="Street number & name"
+            type="text"
+            size="small"
+            margin="dense"
+            error={!!errors.tax_address1}
+            helperText={errors.tax_address1 ? errors.tax_address1.message : ""}
+            />
+        )}/>
+
+      <Controller name="tax_address2" control={control} render={({ field }) => (
+          <TextField 
+            {...field} 
+            label="5. Address line 2"
+            variant="outlined"
+            placeholder="Apt. number"
+            type="text"
+            size="small"
+            margin="dense"
+            error={!!errors.tax_address2}
+            helperText={errors.tax_address2 ? errors.tax_address2.message : ""}
+            />
+        )}/>
+
+      <Controller name="tax_city" control={control} render={({ field }) => (
+          <TextField 
+            {...field} 
+            label="6. City"
+            variant="outlined"
+            placeholder="San Diego"
+            type="text"
+            size="small"
+            margin="dense"
+            error={!!errors.tax_city}
+            helperText={errors.tax_city ? errors.tax_city.message : ""}
+            />
+        )}/>
+
+        <Controller name="tax_state" control={control} render={({ field }) => (
+          <TextField 
+            {...field} 
+            label="6. State"
+            variant="outlined" 
+            placeholder="CA"
+            type="text"
+            size="small"
+            margin="dense"
+            error={!!errors.tax_state}
+            helperText={errors.tax_state ? errors.tax_state.message : ""}
+            />
+        )}/>
+
+        <Controller name="tax_zip" control={control} render={({ field }) => (
+          <TextField 
+            {...field} 
+            label="6. Zip"
+            variant="outlined"
+            placeholder="12345"
+            type="text"
+            size="small"
+            margin="dense"
+            error={!!errors.tax_zip}
+            helperText={errors.tax_zip ? errors.tax_zip.message : ""}
+            />
+        )}/>
+
+        <Controller name="taxID" control={control} render={({ field }) => (
+          <TextField 
+            {...field} 
+            label="TIN (Taxpayer Identification Number"
+            variant="outlined"
+            placeholder="SSN/EIN"
+            type="text"
+            size="small"
+            margin="dense"
+            error={!!errors.taxID}
+            helperText={errors.taxID ? errors.taxID.message : ""}
+            />
+        )}/>
+
+      <div>
+        <ol>
+          <strong>By saving my information here, I submit under penalty of perjury that:</strong>
+          <li>My SSN or TIN above is correct.</li>
+          <li>I am a U.S. person.</li>
+          <li>I am not subject to backup withholding tax.</li>
+          <li>I am not exempt from FATCA reporting.</li>
+        </ol>
+      </div>
     </div>
   );
 }
 
-export default UserTaxInfo;
+const mapStateToProps = (state) => {
+  return {
+    loading: state.loading,
+    users: state.users,
+    user: state.user,
+  };
+};
+
+export default connect(mapStateToProps, { fetchUserData, editUser })(UserTaxInfo);
