63eb156f5af8d9898a93c4527b1c0f24d065be22
elijahsamuels <elijahsamuels@gmail.com>
2021-10-25 22:21:25 -0700

    clean up. Added StaffInfo, UserTaxInfo and UserPhoto components to CreateUser

--- src/pages/CreateUser.js
+++ src/pages/CreateUser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 // import { Link } from "react-router-dom";
 import { createNewUser } from "../actions/userActions";
@@ -6,26 +6,30 @@ import UserName from "../components/userComponents/UserName";
 import UserContact from "../components/userComponents/UserContact";
 import UserAddress from "../components/userComponents/UserAddress";
 import UserBio from "../components/userComponents/UserBio";
+import UserTaxInfo from "../components/userComponents/UserTaxInfo";
 import UserPaymentInfo from "../components/userComponents/UserPaymentInfo";
+import StaffInfo from "../components/userComponents/StaffInfo";
+import UserPhoto from "../components/userComponents/UserPhoto";
 
-// import { schema } from "../components/userComponents/UserSchema";
+import { schema } from "../components/userComponents/UserSchema";
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import LoadingButton from '@mui/lab/LoadingButton';
 // import TextField from '@mui/material/TextField';
 
 import { useForm, FormProvider } from "react-hook-form";
-// import { yupResolver } from "@hookform/resolvers/yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 function CreateUser(props) {
 
+  
   const methods = useForm({
-    // resolver: yupResolver(schema),
-    defaultValues: {
-      first_name: "",
-      last_name: "",
-      phone: "",
-      email: "",
-      }
+    resolver: yupResolver(schema),
+    // defaultValues: {
+    //   first_name: "",
+    //   last_name: "",
+    //   phone: "",
+    //   email: "",
+    //   }
     });
   
   const onHandleSubmit = (data) => {
@@ -38,11 +42,15 @@ function CreateUser(props) {
     <div className="createNewUser">
       <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(onHandleSubmit)}>
-          <UserName />       
-          <UserContact />       
-          <UserAddress />       
-          <UserBio />       
-          <UserPaymentInfo />       
+          <UserPhoto />
+          <UserName />
+          <UserContact />
+          <UserAddress />
+          <UserBio />
+          <UserPaymentInfo />
+          <StaffInfo />
+          <UserTaxInfo />
+               <br />
           <LoadingButton
             color="primary"
             loadingPosition="start"
