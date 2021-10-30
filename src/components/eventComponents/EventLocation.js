86be6ea2f6804b8154d512f52a30ee20e491c5d8
elijahsamuels <elijahsamuels@gmail.com>
2021-10-30 15:47:12 -0700

    updated status and inddor (boolean) to select options instead of textfields

--- src/components/eventComponents/EventLocation.js
+++ src/components/eventComponents/EventLocation.js
@@ -3,6 +3,9 @@ import { connect } from "react-redux";
 import { fetchEventData, editEvent } from "../../actions/eventActions";
 import { Controller, useFormContext } from "react-hook-form";
 import TextField from '@mui/material/TextField';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
 
 function EventLocation (props) {
 // console.log("EventLocation/props.event: ", props.event)
@@ -33,7 +36,7 @@ function EventLocation (props) {
       <Controller name="venue_capacity" control={control} render={({ field }) => (
         <TextField 
         {...field}
-        type="text"
+        type="number"
         label="Venue Capacity"
         variant="outlined" 
         size="small"
@@ -44,30 +47,58 @@ function EventLocation (props) {
       )}/>
 
       <Controller name="status" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Status" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.status}
-        helperText={errors.status ? errors.status.message : ""}
-        />
-      )}/>
+        <span>
+          <InputLabel shrink id="status">Status</InputLabel>
+          <Select
+            {...field}
+            // onChange={e => setStatus(e.target.value)}
+            // value={status} // this works to change the value of the select (and number of musicians), but changes between controlled to uncontrolled. Boo!
+            // renderValue={() => status}
+            label="Status"
+            labelId="status_label"
+            id="status"
+            variant="outlined"
+            size="small"
+            margin="dense"
+            sx={{ m: 0.5, minWidth: 200 }}
+          >
+          <MenuItem value="0" disabled><em>Status</em></MenuItem>
+          <MenuItem value="Inquiry">1. Inquiry</MenuItem>
+          <MenuItem value="Quote">2. Quote Sent</MenuItem>
+          <MenuItem value="Negotiations">3. Negotiations</MenuItem>
+          <MenuItem value="Confirmed">4. Confirmed</MenuItem>
+          <MenuItem value="Green Light">5. Green Light</MenuItem>
+          <MenuItem value="Completed">6. Completed</MenuItem>
+          <MenuItem value="TBD">7. TBD</MenuItem>
+          <MenuItem value="TBD">8. TBD</MenuItem>
+          <MenuItem value="Postponed">9. Postponed</MenuItem>
+          <MenuItem value="Cancelled">10. Cancelled</MenuItem>
+        </Select>
+        </span>
+        )}/>
 
       <Controller name="indoor" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Indoor/Outdoor" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.indoor}
-        helperText={errors.indoor ? errors.indoor.message : ""}
-        />
-      )}/>
+        <span>
+          <InputLabel shrink id="indoor">Indoor/Outdoor</InputLabel>
+          <Select
+            {...field}
+            // onChange={e => setindoor(e.target.value)}
+            // value={indoor} // this works to change the value of the select (and number of musicians), but changes between controlled to uncontrolled. Boo!
+            // renderValue={() => indoor}
+            label="indoor"
+            labelId="indoor_label"
+            id="indoor"
+            variant="outlined"
+            size="small"
+            margin="dense"
+            sx={{ m: 0.5, minWidth: 200 }}
+          >
+          <MenuItem value="0" disabled><em>Indoor/Outdoor</em></MenuItem>
+          <MenuItem value="true">1. Indoor</MenuItem>
+          <MenuItem value="false">2. Outdoor</MenuItem>
+        </Select>
+        </span>
+        )}/>
 
       <Controller name="address1" control={control} render={({ field }) => (
         <TextField 
