b37b0e6745e7535a2f443ef89f06f6ffe61a7f0f
elijahsamuels <elijahsamuels@gmail.com>
2021-10-30 14:05:25 -0700

    updated EventMusicians: each musician has a payrate. Payrate value is a temporary placeholder. Need to fetch this value from the Gigs table or add new fields to the Events table

--- src/components/eventComponents/EventMusicians.js
+++ src/components/eventComponents/EventMusicians.js
@@ -6,46 +6,71 @@ import TextField from '@mui/material/TextField';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import InputLabel from '@mui/material/InputLabel';
-
-
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
 
 function EventMusicians (props) {
-  console.log("props.event", props.event)
+  console.log("typeof (props.event.band_size)", typeof (props.event.band_size))
   // console.log("EventContact/props.event: ", props.event)
-  const { control, formState: { errors }} = useFormContext({
+  const { watch, control, formState: { errors }} = useFormContext({
     defaultValues: props.event,
   });
 
-  const [bandSize, setBandSize] = useState();
-  
-  useEffect(() => {
-    // console.log("props.event", props.event)
-  }, [bandSize]);
+  const [bandSize, setBandSize] = useState(props.event.band_size);
+  const musicianPayRate = 300
 
-  
+  // useEffect(() => {
+  //   // console.log("props.event", props.event)
+  //   setBandSize(props.event.band_size)
+  // }, [bandSize]);
+
   const musicianCountGenerator = () => {
+    console.log("bandSize", bandSize)
+    console.log("watch('band_size')", watch("band_size"))
     
     let musicianCountFromBandSizeArray = [];
-    console.log("bandSize", bandSize)
-    for (let i = 1; i < (+bandSize+1); i++) {
+    for (let i = 1; i < (parseInt(bandSize)+1); i++) {
       musicianCountFromBandSizeArray.push(
-      <div>
-        <Controller name={`musician_0${i}`} control={control} render={({ field }) => (
-          <TextField 
-            {...field}
-            type="text"
-            label={`Musician 0${i}`}
-            variant="outlined" 
-            size="small"
-            margin="dense"
-            error={!!errors[`musician0${i}`]}
-            helperText={errors[`musician0${i}`] ? errors[`musician0${i}`].message : ""}
-            />
-          )}/>
-      </div>
+        <div>
+          <Controller key={`musician_${i}`} name={`musician_${i}`} control={control} render={({ field }) => (
+            console.log(`Musician ${i}/field`, field),
+            <TextField 
+              {...field}
+              type="text"
+              label={`Musician ${i}`}
+              variant="outlined" 
+              size="small"
+              margin="dense"
+              error={!!errors[`musician_${i}`]}
+              helperText={errors[`musician_${i}`] ? errors[`musician_${i}`].message : ""}
+              />
+            )}/>
+
+          <Controller key={`musician_${i}_pay_rate`} name={`musician_${i}_pay_rate`} control={control} startAdornment={<InputAdornment position="start">$</InputAdornment>} render={({ field }) => (
+            console.log(`Musician ${i} Pay Rate/field`, field),
+            <TextField 
+              {...field}
+              type="number"
+              label={`Pay (USD)`}
+              placeholder={`Pay (USD)`}
+              value={musicianPayRate}
+              variant="outlined" 
+              size="small"
+              margin="dense"
+              error={!!errors[`musician_${i}_pay_rate`]}
+              helperText={errors[`musician_${i}_pay_rate`] ? errors[`musician_${i}_pay_rate`].message : ""}
+              />
+            )}/>
+        </div>
       );
     }
     return musicianCountFromBandSizeArray;
+  }
+
+  const bandTotalCost = () => {
+    let bandCost = 20100
+
+    return `$${bandCost}`
   }
 
   return (
@@ -59,233 +84,62 @@ function EventMusicians (props) {
         <span>
           <InputLabel id="band_size">Band Size</InputLabel>
           <Select
-          {...field}
-          onChange={e => setBandSize(e.target.value)}
-          label="Band Size"
-          id="band_size"
-          variant="outlined"
-          size="small"
-          margin="dense"
-          sx={{ minWidth: 120 }}
+            {...field}
+            onChange={e => setBandSize(e.target.value)}
+            // value={bandSize} // this works to change the value of the select (and number of musicians), but changes between controlled to uncontrolled. Boo!
+            renderValue={() => bandSize}
+            label="Band Size"
+            id="band_size"
+            variant="outlined"
+            size="small"
+            margin="dense"
+            sx={{ m: 0.5, minWidth: 50 }}
           >
-          <MenuItem value="" disabled><em>Band Size</em></MenuItem>
-          <MenuItem value="1">1</MenuItem>
-          <MenuItem value="2">2</MenuItem>
-          <MenuItem value="3">3</MenuItem>
-          <MenuItem value="4">4</MenuItem>
-          <MenuItem value="5">5</MenuItem>
-          <MenuItem value="6">6</MenuItem>
-          <MenuItem value="7">7</MenuItem>
-          <MenuItem value="8">8</MenuItem>
-          <MenuItem value="9">9</MenuItem>
-          <MenuItem value="10">10</MenuItem>
-          <MenuItem value="11">11</MenuItem>
-          <MenuItem value="12">12</MenuItem>
-          <MenuItem value="13">13</MenuItem>
-          <MenuItem value="14">14</MenuItem>
-          <MenuItem value="15">15</MenuItem>
+          <MenuItem value="0" disabled><em>Band Size</em></MenuItem>
+          {/* should refactor the MenuItems to be generated instead of manually written */}
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+          <MenuItem value={4}>4</MenuItem>
+          <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={6}>6</MenuItem>
+          <MenuItem value={7}>7</MenuItem>
+          <MenuItem value={8}>8</MenuItem>
+          <MenuItem value={9}>9</MenuItem>
+          <MenuItem value={10}>10</MenuItem>
+          <MenuItem value={11}>11</MenuItem>
+          <MenuItem value={12}>12</MenuItem>
+          <MenuItem value={13}>13</MenuItem>
+          <MenuItem value={14}>14</MenuItem>
+          <MenuItem value={15}>15</MenuItem>
         </Select>
+        <Controller name={'band_cost'} control={control} render={({ field }) => (
+          // console.log(`bandCost`, field),
+          <TextField 
+            {...field}
+            // type="text"
+            label="Band Cost"
+            variant="outlined" 
+            size="small"
+            margin="dense"
+            sx={{ m: 0.5, maxWidth: 90 }}
+            // sx={{ m: 0.5, width: '12ch' }}
+            InputProps={{
+              readOnly: true,
+              value: bandTotalCost(),
+            }}
+            // error={!!errors[`bandCost`]}
+            // // helperText={errors[`bandCost`] ? errors[`bandCost`].message : ""}
+            />
+          )}/>
+
+
+        {/* {bandTotalCost()} */}
+
         </span>
         )}/>
 
         {musicianCountGenerator()}
-
-      {/*
-      <Controller name="musician01" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 01" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician01}
-        helperText={errors.musician01 ? errors.musician01.message : ""}
-        />
-      )}/>
-
-      <Controller name="musician02" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 02" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician02}
-        helperText={errors.musician02 ? errors.musician02.message : ""}
-        />
-      )}/>
-
-      <Controller name="musician03" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 03" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician03}
-        helperText={errors.musician03 ? errors.musician03.message : ""}
-        />
-      )}/>
-
-      <Controller name="musician04" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 04" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician04}
-        helperText={errors.musician04 ? errors.musician04.message : ""}
-        />
-      )}/>
-
-      <Controller name="musician05" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 05" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician05}
-        helperText={errors.musician05 ? errors.musician05.message : ""}
-        />
-      )}/>
-
-      <Controller name="musician06" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 06" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician06}
-        helperText={errors.musician06 ? errors.musician06.message : ""}
-        />
-      )}/>
-
-      <Controller name="musician07" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 07" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician07}
-        helperText={errors.musician07 ? errors.musician07.message : ""}
-        />
-      )}/>
-
-      <Controller name="musician08" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 08" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician08}
-        helperText={errors.musician08 ? errors.musician08.message : ""}
-        />
-      )}/>
-
-      <Controller name="musician09" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 09" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician09}
-        helperText={errors.musician09 ? errors.musician09.message : ""}
-        />
-      )}/>
-
-      <Controller name="musician10" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 10" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician10}
-        helperText={errors.musician10 ? errors.musician10.message : ""}
-        />
-      )}/>
-
-      <Controller name="musician11" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 11" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician11}
-        helperText={errors.musician11 ? errors.musician11.message : ""}
-        />
-      )}/>
-
-      <Controller name="musician12" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 12" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician12}
-        helperText={errors.musician12 ? errors.musician12.message : ""}
-        />
-      )}/>
-
-      <Controller name="musician13" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 13" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician13}
-        helperText={errors.musician13 ? errors.musician13.message : ""}
-        />
-      )}/>
-
-      <Controller name="musician14" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 01" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician14}
-        helperText={errors.musician14 ? errors.musician14.message : ""}
-        />
-      )}/>
-
-      <Controller name="musician15" control={control} render={({ field }) => (
-        <TextField 
-        {...field}
-        type="text"
-        label="Musician 15" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        error={!!errors.musician15}
-        helperText={errors.musician15 ? errors.musician15.message : ""}
-        />
-      )}/>
-*/}
 
     </div>
   );
