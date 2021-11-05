c435b42e738c8d76b74015afcc6575a1f8d65820
elijahsamuels <elijahsamuels@gmail.com>
2021-11-05 13:11:38 -0600

    Musicians can be added/removed based on band_size Select. Still has a memory leak, but everything functions properly.

--- src/components/eventComponents/EventMusicians.js
+++ src/components/eventComponents/EventMusicians.js
@@ -19,15 +19,25 @@ function EventMusicians (props) {
   });
 
   const basePayRate = 300 // temp value
-
   const [bandCostSummed, setBandCostSummed] = useState(0);
   const [musicianPayRate, setMusicianPayRate] = useState(basePayRate ? basePayRate : 0);
   const [bandSize, setBandSize] = useState(props.event.band_size);
 
+  // useEffect(() => {
+  //   return () => {
+  //     console.log(props.event.band_size);
+  //   };
+  // }, [setBandSize]);
+
+
+
+
+
+
   // going to leave this unfinished. Need to add values to the database first, and THEN read them
   const sendPayRateToBeSummed = (payRate) => {
     let summedCost = []
-    console.log("bandCostSummed: ", bandCostSummed, "payRate: ", payRate)
+    // console.log("bandCostSummed: ", bandCostSummed, "payRate: ", payRate)
     summedCost.push(payRate)
     summedCost.reduce((a,b) => a+b)
     setBandCostSummed(summedCost)
@@ -40,21 +50,13 @@ function EventMusicians (props) {
   // }, [bandCostSummed]);
 
 
-  // useEffect(() => {
-  //   // console.log("props.event", props.event)
-  //   setBandSize(props.event.band_size)
-  // }, [bandSize]);
-
   const musicianCountGenerator = () => {
-    // console.log("bandSize", bandSize)
-    // console.log("watch('band_size')", watch("band_size"))
     
     let musicianCountFromBandSizeArray = [];
-    for (let i = 1; i < (parseInt(bandSize)+1); i++) {
+    for (let i = 1; i < (parseInt(bandSize)+1 || props.event.band_size+1); i++) {
       musicianCountFromBandSizeArray.push(
-        <div key={`musician_${i}`}>
+        <div key={`musician_${i}_details`}>
           <Controller name={`musician_${i}`} control={control} render={({ field }) => (
-            console.log(`Musician ${i}/field`, field),
             <TextField 
               {...field}
               type="text"
@@ -63,19 +65,20 @@ function EventMusicians (props) {
               variant="outlined" 
               size="small"
               margin="dense"
+              sx={{ ml: 0.5}}
               error={!!errors[`musician_${i}`]}
               helperText={errors[`musician_${i}`] ? errors[`musician_${i}`].message : ""}
               />
             )}/>
 
           <Controller name={`musician_${i}_pay_rate`} control={control} startAdornment={<InputAdornment position="start">$</InputAdornment>} render={({ field }) => (
-            console.log(`Musician ${i} Pay Rate/field`, field),
+            // console.log(`Musician ${i} Pay Rate/field`, field),
             <TextField 
               {...field}
               type="number"
               key={`musician_${i}_pay_rate_text`}
-              label={`Pay (USD)`}
-              placeholder={`Pay (USD)`}
+              label="Pay (USD)"
+              placeholder="Pay (USD)"
               InputProps={{
                 startAdornment: <InputAdornment position="start">$</InputAdornment>,
               }}
@@ -95,7 +98,7 @@ function EventMusicians (props) {
             <TextField 
             {...field}
             type="text"
-            label={`Invoice Received?`} 
+            label="Invoice Received?" 
             variant="outlined" 
             size="small"
             margin="dense"
@@ -109,7 +112,7 @@ function EventMusicians (props) {
             <TextField 
             {...field}
             type="text"
-            label={`Paid?`} 
+            label="Paid?" 
             variant="outlined" 
             size="small"
             margin="dense"
@@ -118,7 +121,6 @@ function EventMusicians (props) {
             helperText={errors[`musician_${i}_invoice_paid`] ? errors[`musician_${i}_invoice_paid`].message : ""}
             />
           )}/>
-
         </div>
       );
     }
@@ -140,74 +142,81 @@ function EventMusicians (props) {
         Musicians Component
       </h3>
 
-      <Controller name="band_size" control={control} render={({ field }) => (
-        <div>
-          <FormControl sx={{ ml: 0.5, mt: 0.5, minWidth: 50 }}>
-          <InputLabel id="band_size">Size</InputLabel>
-            <Select
+      <Controller 
+        name="band_size" 
+        control={control} 
+        render={({ field }) => (
+          <div>
+            {/* <FormControl sx={{ ml: 0.5, mt: 0.5, minWidth: 50 }}> */}
+            {/* <InputLabel id="band_size">Size</InputLabel> */}
+              <TextField
+                {...field}
+                label="Size"
+                id="band_size"
+                select
+                onChange={(e) => {field.onChange(e), setBandSize(e.target.value)}} // This is working to update the page and save to the database. It's still throwing a "unmount/memory leak in your application" error.
+                variant="outlined"
+                size="small"
+                margin="dense"
+                sx={{ ml: 0.5, mt: 0.5, minWidth: 100 }}
+              >
+              {/* <Select
+                {...field}
+                onChange={e => setBandSize(e.target.value)}
+                // value={() => bandSize} // this works to change the value of the select (and number of musicians), but changes between controlled to uncontrolled. Boo!
+                renderValue={bandSize}
+                label="Size"
+                id="band_size"
+                variant="outlined"
+                size="small"
+                margin="dense"
+              > */}
+
+              <MenuItem value="0" disabled><em>Band Size</em></MenuItem>
+              {/* should refactor the MenuItems to be generated instead of manually written */}
+              <MenuItem value="1">1</MenuItem>
+              <MenuItem value="2">2</MenuItem>
+              <MenuItem value="3">3</MenuItem>
+              <MenuItem value="4">4</MenuItem>
+              <MenuItem value="5">5</MenuItem>
+              <MenuItem value="6">6</MenuItem>
+              <MenuItem value="7">7</MenuItem>
+              <MenuItem value="8">8</MenuItem>
+              <MenuItem value="9">9</MenuItem>
+              <MenuItem value="10">10</MenuItem>
+              <MenuItem value="11">11</MenuItem>
+              <MenuItem value="12">12</MenuItem>
+              <MenuItem value="13">13</MenuItem>
+              <MenuItem value="14">14</MenuItem>
+              <MenuItem value="15">15</MenuItem>
+            </TextField>
+          {/* </FormControl> */}
+
+          {/* <Controller name={'band_cost'} control={control} render={({ field }) => ( */}
+            {/* // console.log(`bandCostSummedFromMusicianPayRates`, field), */}
+            <TextField 
               {...field}
-              onChange={e => setBandSize(e.target.value)}
-              // value={bandSize} // this works to change the value of the select (and number of musicians), but changes between controlled to uncontrolled. Boo!
-              renderValue={() => bandSize}
-              label="Size"
-              id="band_size"
-              variant="outlined"
+              // type="text"
+              label="Band Cost"
+              variant="outlined" 
               size="small"
               margin="dense"
-            >
+              sx={{ m: 0.5, maxWidth: 90 }}
+              // sx={{ m: 0.5, width: '12ch' }}
+              InputProps={{
+                readOnly: true,
+                value: bandCostSummed,
+                startAdornment: <InputAdornment position="start">$</InputAdornment>,
 
-            <MenuItem value="0" disabled><em>Band Size</em></MenuItem>
-            {/* should refactor the MenuItems to be generated instead of manually written */}
-            <MenuItem value="1">1</MenuItem>
-            <MenuItem value="2">2</MenuItem>
-            <MenuItem value="3">3</MenuItem>
-            <MenuItem value="4">4</MenuItem>
-            <MenuItem value="5">5</MenuItem>
-            <MenuItem value="6">6</MenuItem>
-            <MenuItem value="7">7</MenuItem>
-            <MenuItem value="8">8</MenuItem>
-            <MenuItem value="9">9</MenuItem>
-            <MenuItem value="10">10</MenuItem>
-            <MenuItem value="11">11</MenuItem>
-            <MenuItem value="12">12</MenuItem>
-            <MenuItem value="13">13</MenuItem>
-            <MenuItem value="14">14</MenuItem>
-            <MenuItem value="15">15</MenuItem>
-          </Select>
-        </FormControl>
-
-        {/* <Controller name={'band_cost'} control={control} render={({ field }) => ( */}
-          {/* // console.log(`bandCostSummedFromMusicianPayRates`, field), */}
-          <TextField 
-            {...field}
-            // type="text"
-            label="Band Cost"
-            variant="outlined" 
-            size="small"
-            margin="dense"
-            sx={{ m: 0.5, maxWidth: 90 }}
-            // sx={{ m: 0.5, width: '12ch' }}
-            InputProps={{
-              readOnly: true,
-              value: bandCostSummed,
-              startAdornment: <InputAdornment position="start">$</InputAdornment>,
-
-            }}
-            // error={!!errors[`bandCostSummedFromMusicianPayRates`]}
-            // // helperText={errors[`bandCostSummedFromMusicianPayRates`] ? errors[`bandCostSummedFromMusicianPayRates`].message : ""}
-            />
-          {/* // )}/> */}
-
-        {/* {bandTotalCostSummedFromMusicianPayRates()} */}
-
-
-
-
-        </div>
+              }}
+              // error={!!errors[`bandCostSummedFromMusicianPayRates`]}
+              // // helperText={errors[`bandCostSummedFromMusicianPayRates`] ? errors[`bandCostSummedFromMusicianPayRates`].message : ""}
+              />
+            {/* // )}/> */}
+          {/* {bandTotalCostSummedFromMusicianPayRates()} */}
+          </div>
         )}/>
-
         {musicianCountGenerator()}
-
     </div>
   );
 };
