4f201dafe3ec6948340ab7baed8d61a5ca1daeae
elijahsamuels <elijahsamuels@gmail.com>
2021-12-11 23:42:46 -0700

    disabled select options 7 and 8

--- src/components/eventComponents/EventLocation.js
+++ src/components/eventComponents/EventLocation.js
@@ -21,63 +21,72 @@ function EventLocation (props) {
         Location Component
       </h3>
       
-      <Controller name="venue_name" control={control} render={({ field }) => (
+      <Controller 
+        name="venue_name" 
+        control={control} 
+        render={({ field }) => (
         <TextField 
-        {...field}
-        type="text"
-        label="Venue Name" 
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        sx={{ ml: 0.5}}
-        error={!!errors.venue_name}
-        helperText={errors.venue_name ? errors.venue_name.message : ""}
-        />
-      )}/>
+          {...field}
+          type="text"
+          label="Venue Name" 
+          variant="outlined" 
+          size="small"
+          margin="dense"
+          sx={{ ml: 0.5}}
+          error={!!errors.venue_name}
+          helperText={errors.venue_name ? errors.venue_name.message : ""}
+          />
+        )}/>
 
-      <Controller name="venue_capacity" control={control} render={({ field }) => (
+      <Controller 
+        name="venue_capacity" 
+        control={control} 
+        render={({ field }) => (
         <TextField 
-        {...field}
-        type="number"
-        label="Venue Capacity"
-        variant="outlined" 
-        size="small"
-        margin="dense"
-        sx={{ ml: 0.5 }} 
-        error={!!errors.venue_capacity}
-        helperText={errors.venue_capacity ? errors.venue_capacity.message : ""}
-        />
-      )}/>
+          {...field}
+          type="number"
+          label="Venue Capacity"
+          variant="outlined" 
+          size="small"
+          margin="dense"
+          sx={{ ml: 0.5 }} 
+          error={!!errors.venue_capacity}
+          helperText={errors.venue_capacity ? errors.venue_capacity.message : ""}
+          />
+        )}/>
 
-      <Controller name="status" control={control} render={({ field }) => (
+      <Controller 
+        name="status" 
+        control={control} 
+        render={({ field }) => (
         <span>
           <FormControl sx={{ ml: 0.5, mt: 1, minWidth: 200 }}>
-          <InputLabel id="status">Status</InputLabel>
-          <Select
-            {...field}
-            // onChange={e => setStatus(e.target.value)}
-            // value={status} // this works to change the value of the select (and number of musicians), but changes between controlled to uncontrolled. Boo!
-            // renderValue={() => status}
-            label="Status"
-            id="status"
-            variant="outlined"
-            size="small"
-            margin="dense"
-            // sx={{ ml: 0.5, mt: 1, minWidth: 200 }}
-          >
-          <MenuItem value="0" disabled><em>Status</em></MenuItem>
-          <MenuItem value="Inquiry">1. Inquiry</MenuItem>
-          <MenuItem value="Quote">2. Quote Sent</MenuItem>
-          <MenuItem value="Negotiations">3. Negotiations</MenuItem>
-          <MenuItem value="Confirmed">4. Confirmed</MenuItem>
-          <MenuItem value="Green Light">5. Green Light</MenuItem>
-          <MenuItem value="Completed">6. Completed</MenuItem>
-          <MenuItem value="TBD">7. TBD</MenuItem>
-          <MenuItem value="TBD">8. TBD</MenuItem>
-          <MenuItem value="Postponed">9. Postponed</MenuItem>
-          <MenuItem value="Cancelled">10. Cancelled</MenuItem>
-        </Select>
-        </FormControl>
+            <InputLabel id="status">Status</InputLabel>
+            <Select
+              {...field}
+              // onChange={e => setStatus(e.target.value)}
+              // value={status} // this works to change the value of the select (and number of musicians), but changes between controlled to uncontrolled. Boo!
+              // renderValue={() => status}
+              label="Status"
+              id="status"
+              variant="outlined"
+              size="small"
+              margin="dense"
+              // sx={{ ml: 0.5, mt: 1, minWidth: 200 }}
+            >
+              <MenuItem value="0" disabled><em>Status</em></MenuItem>
+              <MenuItem value="Inquiry">1. Inquiry</MenuItem>
+              <MenuItem value="Quote">2. Quote Sent</MenuItem>
+              <MenuItem value="Negotiations">3. Negotiations</MenuItem>
+              <MenuItem value="Confirmed">4. Confirmed</MenuItem>
+              <MenuItem value="Green Light">5. Green Light</MenuItem>
+              <MenuItem value="Completed">6. Completed</MenuItem>
+              <MenuItem value="TBD" disabled>7. TBD</MenuItem>
+              <MenuItem value="TBD" disabled>8. TBD</MenuItem>
+              <MenuItem value="Postponed">9. Postponed</MenuItem>
+              <MenuItem value="Cancelled">10. Cancelled</MenuItem>
+            </Select>
+          </FormControl>
         </span>
         )}/>
 
