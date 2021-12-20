00e8a23f0bdf42ce70a1db17465f2849e7f11028
elijahsamuels <elijahsamuels@gmail.com>
2021-12-20 15:29:04 -0700

    added CreateEvent route

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import userSamplePhoto from "../images/puresoulpresents.png";
+import PureSoulLogo from "../images/puresoulpresents.png";
 
 function Nav() {
   return (
     <div className="userDetails">
       <Link to="/">
-      <img src={userSamplePhoto} alt="User" width="80" />
+      <img src={PureSoulLogo} alt="User" width="80" />
        </Link>
       <Link to="/">Home </Link>
       {" - "}
       <Link to="/eventlist">Event List</Link>
       {" - "}
+      <Link to="/createevent">Create Event</Link>
+      {" - "}
       <Link to="/userlist">User List</Link>
       {" - "}
       <Link to="/createuser">Create New User</Link>
       {" - "}
-      {/* <Link to="/userdetails">User Details</Link>
-      {" - "} */}
       <Link to="/contact">Contact</Link>
     </div>
   );
