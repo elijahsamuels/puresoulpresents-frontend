cceae1e67516221d235764952509082866e1e8e3
elijahsamuels <elijahsamuels@gmail.com>
2021-12-23 15:02:54 -0700

    added DatePicker from react-multi-date-picker. Trying this instead of DesktopDatePicker from MUI. It's about a 60% smaller. Seems to easier to build too.

--- src/components/eventComponents/EventDate.js
+++ src/components/eventComponents/EventDate.js
@@ -7,10 +7,15 @@ import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import DateAdapter from '@mui/lab/AdapterDateFns';
+import FormControl from '@mui/material/FormControl';
 // import { format, formatDistance, formatRelative, subDays } from 'date-fns'
+import DatePicker from "react-multi-date-picker";
+
+
 
 function EventDate (props) {
 // console.log("EventDate/props.event: ", props.event)
+// console.log("EventDate/props.event.event_date: ", props.event.event_date)
   const { control, formState: { errors }} = useFormContext({
     defaultValues: props.event,
   });
@@ -37,17 +42,21 @@ function EventDate (props) {
       let year = b.getYear() + 1900
       let month = addZeroValue(b.getMonth())
       let date = addZeroValue(b.getDate())
-      return `${year}-${month}-${date}`
+      return `${month}-${date}-${year}`
     }
   }
 
-  // const [value, setValue] = useState(new Date('2014-08-18T21:11:54'));
-  const [value, setValue] = useState(Date(props.event.event_date));
-  // const [value, setValue] = useState(inputDateConverterYYYYMMDD(props.event.event_date));
+  const [eventDate, setEventDate] = useState(props.event.event_date);
+  // const [eventDate, setEventDate] = useState(new Date('2014-05-18T21:11:54'));
+  // const [eventDate, setEventDate] = useState(Date(props.event.event_date));
+  console.log(eventDate)
+  console.log(props.event.event_date)
+  // const [eventDate, setEventDate] = useState(inputDateConverterYYYYMMDD(props.event.event_date));
 
   const handleChange = (newValue) => {
     console.log("newValue.target.value:", newValue.target.value)
-    // setValue(newValue);
+    console.log("newValue:", newValue)
+    setEventDate(newValue);
   };
 
   return (
@@ -57,30 +66,91 @@ function EventDate (props) {
       {/* {console.log(props.event.event_date)} */}
       {/* {console.log("date converter: ", inputDateConverterYYYYMMDD(props.event.event_date))} */}
 
-      <div>DesktopDatePicker</div>
+      <div>DatePicker</div>
+      <br />
+
+      <Controller
+          control={control}
+          name="event_date"
+          rules={{ required: true }} //optional
+          render={({ field }) => (
+            <>
+              <DatePicker
+                {...console.log(eventDate)}
+                value={outputDateConverterYYYYMMDD(eventDate) || ""}
+                onChange={(date) => {
+                  field.onChange(date?.isValid ? date : "");
+                }}
+                format={navigator.language === 'en-US' ? "MM/DD/YYYY" : "YYYY/MM/DD"}
+              />
+            </>
+          )}
+        />
+
+      {/* <div>DesktopDatePicker</div>
       <br />
       <Controller
         name="event_date"
         control={control}
         render={({ field }) => (
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            {
-              <DesktopDatePicker
-                label="Date desktop"
-                inputFormat="MM/dd/yyyy"
-                value={value}
-                minDate={Date.now()}
-                // orientation="landscape"
-                onChange={handleChange}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            }
-          </LocalizationProvider>
+          
+          <TextField 
+            {...field}
+            {...console.log("field:", field)}
+            type="text"
+            label="Event Date"
+            variant="outlined" 
+            size="small"
+            margin="dense"
+            sx={{ ml: 0.5 }}
+            // onChange={(e) => field.onChange(setEventDate(e.target.value))}
+            value={field.value || ''}
+            error={!!errors.venue_capacity}
+            helperText={errors.venue_capacity ? errors.venue_capacity.message : ""}
+          />
+        )}/> */}
+        {/* <br/>
+        <br/>
+        <input
+          type="date"
+          name="event_date"
+          autoComplete="off"
+          // value={eventDate}
+          // value={inputDateConverterYYYYMMDD(eventDate)}
+          value={inputDateConverterYYYYMMDD(eventDate) || ""}
+          // onChange={handleChange}
+          onChange={(e) => handleChange(e)}
+          // value={field.value}
+        /> */}
+
+      {/* <Controller
+        name="event_date"
+        control={control}
+        render={({ field }) => (
+          <FormControl sx={{ ml: 0.5, mt: 1, minWidth: 200 }}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              {
+                <DesktopDatePicker
+                  {...field}
+                  label="Date desktop"
+                  inputFormat="MM/dd/yyyy"
+                  value={eventDate || ""}
+                  // value={eventDate || field.value}
+                  // minDate={Date.now()}
+                  // orientation="landscape"
+                  onChange={handleChange}
+                  renderInput={(params) => <TextField {...params}/>}
+                />
+              }
+            </LocalizationProvider>
+          </FormControl>
         )}
-      />
+      /> */}
+<br />
+<br />
 
       <div>Controller/Input</div>
-      <div>props.event.event_date: {props.event.event_date}</div>
+      {/* <div>inputDateConverterYYYYMMDD: {outputDateConverterYYYYMMDD(props.event.event_date)}</div> */}
       <div>
         inputDateConverterYYYYMMDD:{" "}
         {inputDateConverterYYYYMMDD(props.event.event_date)}
@@ -96,13 +166,13 @@ function EventDate (props) {
             <div>props.event.event_date: {props.event.event_date}</div>
             <div>field.value: {field.value}</div>
             <div>inputDateConverterYYYYMMDD: {inputDateConverterYYYYMMDD(field.value)}</div>
-            <div>value: {value}</div>
+            <div>eventDate: {eventDate}</div>
             <input
               type="date"
               name="event_date"
               autoComplete="off"
-              // value={value}
-              // value={inputDateConverterYYYYMMDD(value)}
+              // value={eventDate}
+              // value={inputDateConverterYYYYMMDD(eventDate)}
               value={inputDateConverterYYYYMMDD(field.value) || ""}
               // onChange={handleChange}
               // onChange={(e) => handleChange(e)}
@@ -134,7 +204,6 @@ function EventDate (props) {
 };
 
 const mapStateToProps = (state) => {
-
   return {
     loading: state.loading,
     users: state.users,
