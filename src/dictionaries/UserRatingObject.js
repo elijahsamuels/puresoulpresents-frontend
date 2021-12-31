e5202d9e16966935de802e43409f1ca8e8d096ef
elijahsamuels <elijahsamuels@gmail.com>
2021-12-30 22:00:15 -0700

    added UserRatingObject

---
+++ src/dictionaries/UserRatingObject.js
@@ -0,0 +1,13 @@
+const UserRatingObject = {
+  1: "Great",
+  2: "Good",
+  3: "Ok",
+  4: "Trainable",
+  5: "Questionable",
+  6: "Difficult",
+  7: "Bad",
+  8: "Hidden - Doesn't want gigs",
+  9: "Hidden - Blacklisted"
+}
+
+export default UserRatingObject
