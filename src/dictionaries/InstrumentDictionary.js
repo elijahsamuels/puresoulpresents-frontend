a4f39f3f81cfcb2a7bbe458a487b3c98d1bd9307
elijahsamuels <elijahsamuels@gmail.com>
2021-12-30 17:48:31 -0700

    added InstrumentDictionary as json file and js object literal

---
+++ src/dictionaries/InstrumentDictionary.js
@@ -0,0 +1,49 @@
+const InstrumentDictionary = {
+  0: "Accordion",
+  1: "Bagpipes",
+  2: "Banjo",
+  3: "Bass (Electric)",
+  4: "Bass (Upright)",
+  5: "Bass (Classical)",
+  6: "Bassoon",
+  7: "Bongo",
+  8: "Clarinet",
+  9: "Congo",
+
+  10: "Timbales",
+  11: "Double bass",
+  12: "Drum kit",
+  13: "Fiddle",
+  14: "Flute",
+  15: "French horn",
+  16: "Guitar (Acoustic)",
+  17: "Guitar (Classical)",
+  18: "Guitar (Electric)",
+  19: "Harmonica",
+
+  20: "Harp",
+  21: "Mandolin",
+  22: "Oboe",
+  23: "Organ",
+  24: "Piano",
+  25: "Bass",
+  26: "Saxophone (Alto)",
+  27: "Saxophone (Tenor)",
+  28: "Saxophone (Baritone)",
+  29: "Trombone",
+
+  30: "Trumpet",
+  31: "Tuba",
+  32: "Ukulele",
+  33: "Vocals (Female - Soprano)",
+  34: "Vocals (Female - Alto)",
+  35: "Vocals (Male - Tenor)",
+  36: "Vocals (Male - Baritone)",
+  37: "Fiddle",
+  38: "Viola",
+  39: "Violin",
+  
+  40: "Cello"
+}
+
+export default InstrumentDictionary
