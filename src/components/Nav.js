bd816109616b2b570a53b5ab0ba8bcc69f6b5d54
elijahsamuels <elijahsamuels@gmail.com>
2022-01-07 01:01:27 -0700

    added nav link for User Gig List

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,19 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PureSoulLogo from "../images/puresoulpresents.png";
 
+let user_id = 1
+
 function Nav() {
   return (
     <div className="userDetails">
       <Link to="/">
       <img src={PureSoulLogo} alt="User" width="80" />
        </Link>
-      <Link to="/">Home </Link>
+      <Link to="/">Home</Link>
       {" - "}
       <Link to="/eventlist">Event List</Link>
       {" - "}
       <Link to="/createevent">Create Event</Link>
       {" - "}
       <Link to="/userlist">User List</Link>
+      {" - "}
+      <Link to={`/users/${user_id}/events`}>User GigList</Link>
       {" - "}
       <Link to="/createuser">Create New User</Link>
       {" - "}
