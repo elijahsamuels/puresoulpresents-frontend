32bdeae5928a37a133c4a4a6f38dba3db057295b
elijahsamuels <elijahsamuels@gmail.com>
2022-01-07 01:06:36 -0700

    return function for instrument dictionary

--- src/components/staticComponents/InstrumentNameReturn.js
+++ src/components/staticComponents/InstrumentNameReturn.js
@@ -1,11 +1,8 @@
 import InstrumentDictionary from "../../dictionaries/InstrumentDictionary"
 
-const InstrumentNameReturn = (instrument_id) => Object.values(InstrumentDictionary)[instrument_id]
-
-// function InstrumentNameReturn(inst_id) {
-//   // const InstrumentNames = (instrument_id) => Object.values(InstrumentDictionary)[instrument_id]
-//   // return (InstrumentNames(inst_id));
-// return Object.values(InstrumentDictionary)[inst_id]
-// }
-
+const InstrumentNameReturn = (instrument_id) => {
+  return InstrumentDictionary[instrument_id]
+}
 export default InstrumentNameReturn;
+
+// return Object.values(InstrumentDictionary)[instrument_id-1] // need to subtract 1 to adjust the array from starting at the zeroth index
