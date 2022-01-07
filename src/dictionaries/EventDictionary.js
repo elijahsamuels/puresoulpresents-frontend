411d2d5e5de427485f0b9770e74b79503994aaf2
elijahsamuels <elijahsamuels@gmail.com>
2022-01-07 01:05:17 -0700

    created EventDictionary file

---
+++ src/dictionaries/EventDictionary.js
@@ -0,0 +1,13 @@
+const EventDictionary = {
+  1: "Wedding",
+  2: "Corporate",
+  3: "Candlelight Concert",
+  4: "Concert",
+  5: "Showcase",
+  6: "Other",
+  7: "TBD",
+  8: "TBD",
+  9: "TBD"
+}
+
+export default EventDictionary
